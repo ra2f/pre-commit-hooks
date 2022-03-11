006b0ef4c3ba211984eaeb29a781e25c45b447c4
root <root@E3D-0022PC-W.ad.emotion3d.ai>
2022-03-11 16:48:23 +0100

    [~] Fix shebangs rules

--- pre_commit_hooks/generic_check_executables_have_shebangs.py
+++ pre_commit_hooks/generic_check_executables_have_shebangs.py
@@ -5,36 +5,81 @@ import sys
 from typing import Optional
 from typing import Sequence
 
+import argparse
+import shlex
+import sys
+from typing import Generator
+from typing import NamedTuple
+from typing import Sequence
 
-def check_has_shebang(path: str) -> int:
-    with open(path, 'rb') as file_handler:
-        first_bytes = file_handler.read(2)
+from pre_commit_hooks.util import cmd_output
+from pre_commit_hooks.util import zsplit
 
-    if first_bytes != b'#!':
-        quoted = shlex.quote(path)
-        print(
-            '{}: marked executable but has no (or invalid) shebang!\n'.format(path),
-            "  If it isn't supposed to be executable, try: ",
-            '`chmod -x {}`\n'.format(quoted),
-            '  If it is supposed to be executable, double-check its shebang.',
-            file=sys.stderr,
-        )
-        return 1
-    return 0
+EXECUTABLE_VALUES = frozenset(('1', '3', '5', '7'))
+
+def check_executables(paths: list[str]) -> int:
+    if sys.platform == 'win32':  # pragma: win32 cover
+        return _check_git_filemode(paths)
+    else:  # pragma: win32 no cover
+        retv = 0
+        for path in paths:
+            if not has_shebang(path):
+                _message(path)
+                retv = 1
+
+        return retv
 
 
-def main(argv: Optional[Sequence[str]] = None) -> int:
+class GitLsFile(NamedTuple):
+    mode: str
+    filename: str
+
+
+def git_ls_files(paths: Sequence[str]) -> Generator[GitLsFile, None, None]:
+    outs = cmd_output('git', 'ls-files', '-z', '--stage', '--', *paths)
+    for out in zsplit(outs):
+        metadata, filename = out.split('\t')
+        mode, _, _ = metadata.split()
+        yield GitLsFile(mode, filename)
+
+
+def _check_git_filemode(paths: Sequence[str]) -> int:
+    seen: set[str] = set()
+    for ls_file in git_ls_files(paths):
+        is_executable = any(b in EXECUTABLE_VALUES for b in ls_file.mode[-3:])
+        if is_executable and not has_shebang(ls_file.filename):
+            _message(ls_file.filename)
+            seen.add(ls_file.filename)
+
+    return int(bool(seen))
+
+
+def has_shebang(path: str) -> int:
+    with open(path, 'rb') as f:
+        first_bytes = f.read(2)
+
+    return first_bytes == b'#!'
+
+
+def _message(path: str) -> None:
+    print(
+        f'{path}: marked executable but has no (or invalid) shebang!\n'
+        f"  If it isn't supposed to be executable, try: "
+        f'`chmod -x {shlex.quote(path)}`\n'
+        f'  If on Windows, you may also need to: '
+        f'`git add --chmod=-x {shlex.quote(path)}`\n'
+        f'  If it is supposed to be executable, double-check its shebang.',
+        file=sys.stderr,
+    )
+
+
+def main(argv: Sequence[str] = None) -> int:
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('filenames', nargs='*')
     args = parser.parse_args(argv)
 
-    retv = 0
-
-    for filename in args.filenames:
-        retv |= check_has_shebang(filename)
-
-    return retv
+    return check_executables(args.filenames)
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    raise SystemExit(main())
